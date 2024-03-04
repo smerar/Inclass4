668b16c317b3f7bd10e18c735ea899a929628eb9
Smera Rockey <Srockey8775@conestogac.on.ca>
2024-03-04 16:54:00 -0500

    Initial commit

---
+++ app/src/main/java/com/example/smera_rockey_8928775_inclass4/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.smera_rockey_8928775_inclass4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
